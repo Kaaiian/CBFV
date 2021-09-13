1f0a53c208147f2cbf8997c3136be0146eea883e
Andrew Falkowski <>
2021-09-13 10:43:19 -0600

    updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cbfv',
-    version='0.1.0',
+    version='1.0.0',
     description='Tool for quickly creating a composition-based feature vector',
     url='https://github.com/kaaiian/CBFV',
     author='Steven Kauwe, Andrew Falkowski, Anthony Wang',
