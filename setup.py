6f32c51bd111f2f9e98bfb8cb12a0b9f8faf96b7
Andrew Falkowski <>
2021-10-14 08:59:42 -0600

    Reverted package naming scheme

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='cbfv',
-    version='1.0.1',
+    name='CBFV',
+    version='1.0.2',
     description='Tool for quickly creating a composition-based feature vector',
     url='https://github.com/kaaiian/CBFV',
     author='Steven Kauwe, Andrew Falkowski, Anthony Wang',
     author_email='jkkauwe@gmail.com',
-    packages=['cbfv'],
+    packages=['CBFV'],
     install_requires=[
                       'numpy',
                       'pandas',
